2b142b6b1d7e3ab11aafff518d48c75fecb53cb3
nishipee <sn.haryo.000@gmail.com>
2021-08-20 20:49:29 +0900

    バリデーションの記述

--- app/models/corporate_user.rb
+++ app/models/corporate_user.rb
@@ -3,4 +3,17 @@ class CorporateUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :building_name
+    validates :phone_num, length: { maximum: 11 }, format: { with: /\A[0-9]+\z/, message: "is invalid. Input only number" }
+  end
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: "is invalid. Include both letters and numbers" }
 end
