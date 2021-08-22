c0d29d3239e15726c86cc54512232719f7195a47
nishipee <sn.haryo.000@gmail.com>
2021-08-22 21:55:06 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :move_to_session, except: :index
+
   def index
   end
 
@@ -15,9 +17,29 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to corporate_user_path(current_corporate_user.id)
+    else
+      render :edit
+    end
+  end
+
   
   private
   def product_params
     params.require(:product).permit(:name, :introduction, :category1_id, :category2_id, :category3_id, :charge_id, :prefecture_id, :scheduled_day_id, :price, :image).merge(corporate_user_id: current_corporate_user.id)
+  end
+
+  def move_to_session
+    @product = Product.find(params[:id])
+    if current_corporate_user != @product.corporate_user
+      redirect_to new_corporate_user_session_path
+    end
   end
 end
