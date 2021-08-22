2f5098fea935e780fb855f8b4face7940bae7893
nishipee <sn.haryo.000@gmail.com>
2021-08-22 15:02:45 +0900

    バリデーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,25 @@
 class Product < ApplicationRecord]
   belongs_to :corporate_user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :charge
+  belongs_to :category
+  belongs_to :scheduled_day
+
+  with_options presence: true, numericality: { other_than: 1, message: "を選択してください" } do
+    validates :prefecture_id
+    validates :category_id
+    validates :charge_id
+    validates :scheduled_day_id
+  end
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+  end
+
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください" }
 end
