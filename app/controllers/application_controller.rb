3e90164ceb566bf51f6e4aa681960fb57e46204b
nishipee <sn.haryo.000@gmail.com>
2021-08-22 16:53:00 +0900

    【ユーザー管理機能】他店舗のページへ遷移することができないように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    case resource
-    when CorporateUser
-      corporate_user_path(resource)
-    end
-  end
 end
