ff797f66239c9656e04ca51104fa53e54533c9bf
nishipee <sn.haryo.000@gmail.com>
2021-08-31 08:40:03 +0900

    購入画面の商品表示

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,4 +1,7 @@
 class PurchaseHistoriesController < ApplicationController
   def index
+    @cart_items = current_cart.cart_items.includes([:product])
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+    @sub_total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_sub_price }
   end
 end
