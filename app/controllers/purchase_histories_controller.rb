a4ceef821af973891a7fe4c7a2833b9d35d7d586
nishipee <sn.haryo.000@gmail.com>
2021-09-01 15:48:48 +0900

    エラーメッセージの日本語化

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -5,5 +5,21 @@ class PurchaseHistoriesController < ApplicationController
     @cart_items = current_cart.cart_items.includes([:product])
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
     @sub_total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_sub_price }
+    @purchase_history_address = PurchaseHistoryAddress.new
+  end
+
+  def create
+    @purchase_history_address = PurchaseHistoryAddress.new(purchase_history_params)
+    if @purchase_history_address.valid?
+      @purchase_history_address.save
+      redirect_to root_path
+    else
+      redirect_to purchase_histories_path, flash: { alert: @purchase_history_address.errors.full_messages }
+    end
+  end
+
+  private
+  def purchase_history_params
+    params.require(:purchase_history_address).permit(:postcode, :prefecture_id, :city, :house_number, :building_name, :phone_num, :total_price, :total_charge).merge(user_id: current_user.id)
   end
 end
--- app/models/purchase_history_address.rb
+++ app/models/purchase_history_address.rb
@@ -1,6 +1,6 @@
 class PurchaseHistoryAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :house_number, :building_name, :phone_num, :total_price, :total_charge, :user_id, :product_id, :purchase_history_id
+  attr_accessor :postcode, :prefecture_id, :city, :house_number, :building_name, :phone_num, :total_price, :total_charge, :user_id, :purchase_history_id
 
   with_options presence: true do
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフンを含む半角数字で入力してください" }
@@ -11,9 +11,12 @@ class PurchaseHistoryAddress
     validates :total_price
     validates :total_charge
     validates :user_id
-    validates :product_id
+    validates :purchase_history_id
   end
 
   def save
-    purchase_history = PurchaseHistory.create(total_price: total_price, total_charge: total_charge, user_id: user_id, product_id: product_id)
+    purchase_history = PurchaseHistory.create(total_price: total_price, total_charge: total_charge, user_id: user_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, house_number: house_number, phone_num: phone_num, purchase_history_id: purchase_history.id)
+  end
+  
 end
