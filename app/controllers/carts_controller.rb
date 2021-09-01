dbd56afe3e62facebd1a86b80c3d364fe43ce001
nishipee <sn.haryo.000@gmail.com>
2021-09-01 15:56:41 +0900

    合計額の表示

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,8 +5,8 @@ class CartsController < ApplicationController
   # カート内アイテムの表示
   def my_cart
     @cart_items = current_cart.cart_items.includes([:product])
-    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
     @sub_total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_sub_price }
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price } + @sub_total
   end
 
   # アイテムの追加
--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -3,8 +3,8 @@ class PurchaseHistoriesController < ApplicationController
   
   def index
     @cart_items = current_cart.cart_items.includes([:product])
-    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
     @sub_total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_sub_price }
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price } + @sub_total
     @purchase_history_address = PurchaseHistoryAddress.new
   end
 
