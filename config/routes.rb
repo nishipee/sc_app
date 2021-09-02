5147bd8b34283ce67fd4c61f270433a49d3d92b0
nishipee <sn.haryo.000@gmail.com>
2021-09-03 01:23:16 +0900

    管理者ページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
+    sessions: "admins/sessions"
   }
 
   devise_for :users, controllers: {
@@ -10,16 +10,16 @@ Rails.application.routes.draw do
   }
 
   devise_for :corporate_users, controllers: {
-    sessions: 'corporate_users/sessions',
-    passwords: 'corporate_users/passwords',
-    registrations: 'corporate_users/registrations'
+    sessions: "corporate_users/sessions",
+    passwords: "corporate_users/passwords",
+    registrations: "corporate_users/registrations"
   }
   root to: "products#index"
 
   resources :products do
-    post '/add_item', to: 'carts#add_item'
-    patch '/update_item', to: 'carts#update_item'
-    delete '/delete_item', to: 'carts#delete_item'
+    post "/add_item", to: "carts#add_item"
+    patch "/update_item", to: "carts#update_item"
+    delete "/delete_item", to: "carts#delete_item"
   end
   
   resources :admins
@@ -28,9 +28,11 @@ Rails.application.routes.draw do
   resources :carts
   resources :purchase_histories, only: [:index, :create]
 
-  get '/corporate_users_showinfo/:id', to: 'corporate_users#showinfo'
+  get "/corporate_users_showinfo/:id", to: "corporate_users#showinfo"
   get "/recent_products", to: "products#recent_products"
   get "/popular_products", to: "products#popular_products"
-  get '/my_cart', to: 'carts#my_cart'
+  get "/my_cart", to: "carts#my_cart"
   get "/order_completed", to: "purchase_histories#order_completed"
+  
+  get "/show_c_user/:corporate_user_id", to: "admins#show_c_user"
 end
