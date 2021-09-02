c9a00432836cf642df999295a71b2d356c75a014
nishipee <sn.haryo.000@gmail.com>
2021-09-03 02:43:39 +0900

    モデル単体テストコード

--- spec/factories/sc_groups.rb
+++ spec/factories/sc_groups.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :sc_group do
+    name          { "testtest" }
+    introduction  { "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest" }
+    how_donation  { "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest" }
+    association :admin
     
+    after(:build) do |sc_group|
+      sc_group.image.attach(io: File.open("public/images/耳をすませば.jpeg"), filename: "耳をすませば.jpeg")
+    end
   end
 end
