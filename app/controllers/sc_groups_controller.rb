684c4f9ec7420c69a12f1b130d88dfa468fcf590
nishipee <sn.haryo.000@gmail.com>
2021-09-03 02:03:09 +0900

    登録画面挿入、コントローラー記述

--- app/controllers/sc_groups_controller.rb
+++ app/controllers/sc_groups_controller.rb
@@ -2,8 +2,20 @@ class ScGroupsController < ApplicationController
   before_action :authenticate_admin!
 
   def new
+    @sc_group = ScGroup.new
   end
 
   def create
+    @sc_group = ScGroup.new(sc_group_params)
+    if @sc_group.save
+      redirect_to admins_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def sc_group_params
+    params.require(:sc_group).permit(:name, :introduction, :how_donation).merge(admin_id: current_admin.id)
   end
 end
