3d0ae2c4eb2bcc0aaebb5fab86d50f059cec3933
nishipee <sn.haryo.000@gmail.com>
2021-09-03 02:06:06 +0900

    エラーメッセージ日本語化

--- app/models/sc_group.rb
+++ app/models/sc_group.rb
@@ -3,6 +3,7 @@ class ScGroup < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :introduction
     validates :how_donation
