1ed186d7495d011c5d312bafee3120e411e60b3f
nishipee <sn.haryo.000@gmail.com>
2021-09-03 01:50:02 +0900

    バリデーション、アソシエーション

--- app/models/sc_group.rb
+++ app/models/sc_group.rb
@@ -1,4 +1,10 @@
 class ScGroup < ApplicationRecord
+  belongs_to :admin
+  has_one_attached :image
 
-  
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :how_donation
+  end
 end
