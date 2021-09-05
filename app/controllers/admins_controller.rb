5dd8744d08236a4715693f48f16e4b26870694bf
nishipee <sn.haryo.000@gmail.com>
2021-09-06 02:29:06 +0900

    管理者ページに総寄付額の表示

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,8 +5,9 @@ class AdminsController < ApplicationController
     @products = Product.includes(:corporate_user)
     @sales = @products.inject(0) { |sum, item| sum + item.sum_of_sales }
 
-    @corporate_users = CorporateUser.all
+    @corporate_users = CorporateUser.all.order("created_at DESC")
     @sc_groups = ScGroup.all
+    donation_price
   end
 
   def show_c_user
@@ -15,5 +16,15 @@ class AdminsController < ApplicationController
 
   def show_sc_group
     @sc_group = ScGroup.find(params[:sc_group_id])
+  end
+
+  private
+
+  def donation_price
+    @donation = []
+    @sc_groups.each_with_index do |sc_g|
+      @donation_histoy = DonationHistory.where(sc_group_id: sc_g.id)
+      @donation << @donation_histoy.inject(0) { |sum, item| sum + item.points }
+    end
   end
 end
