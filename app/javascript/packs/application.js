e326f99e9a86991742d6001417908d7876bdb9d0
nishipee <sn.haryo.000@gmail.com>
2021-09-06 06:32:53 +0900

    プレビュー機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../product_profit")
 require("../card")
+require("../preview")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
